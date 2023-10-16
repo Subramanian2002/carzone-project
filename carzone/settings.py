e4e05eb1bc591caa177542baf88fe0db72d5efc9
Subramanian2002 <subramanian638545@gmail.com>
2023-10-16 21:30:25 +0530

    pages

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Mani@2002',
+        'HOST': 'localhost',
     }
 }
+
 
 
 # Password validation
